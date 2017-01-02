2193c39dad689df28b55ed077c29efcd010f207a
Jan Beinke <git@janbeinke.com>
2017-01-02 23:45:29 +0100

    Fix typos

--- color.go
+++ color.go
@@ -1,12 +1,12 @@
 package pixelflut
 
-// Color stores an RGBA Color
+// Color stores an RGBA Color.
 type Color interface {
 
 	// RGBA returns the color with premultiplied alpha values.
 	// You might want to bit-shift the r, g and b values >> 8 before use.
 	RGBA() (r, g, b, a uint32)
 
-	// HexString returns the hex representation of the color
+	// HexString returns the hex representation of the color.
 	HexString() string
 }
